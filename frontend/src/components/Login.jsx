394dde6c2b45d0db8f392b6776b4fe3cd1f768b8
Bikram Dutta <duttabikram260@gmail.com>
2023-02-10 02:41:29 +0530

    login,signup page and menu bar added with responsiveness

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,57 +1,98 @@
-import React from "react";
-import { Box, Button, Flex, FormLabel, Heading, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormLabel,
+  Heading,
+  IconButton,
+  Image,
+  Input,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import xsell_logo from "../resource/X-new.png";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Login = () => {
+  let [theme, setTheme] = useState(false);
+
+
   return (
     <Box
-      p="20px"
-      bgGradient="linear(to-l,#FF0080,#7928CA)"
-      boxShadow="dark-lg"
-      // border="1px solid"
-      borderRadius={10}
-      pos={"absolute"}
-      top="5rem"
-      left={"35rem"}
+      border={"2px solid"}
+      display="flex"
+      alignItems={"center"}
+      flexDir={"column"}
+      gap={{base:30,sm:30,md:'35px'}}
+      pt={5}
+      h={'100vh'}
+      bgColor={theme? "blackAlpha.900":"whiteAlpha.500"}
     >
-      <Heading size={"lg"} textAlign={"center"} mb={5} color="white">
-        Login
-      </Heading>
-      <Flex flexDir={"column"} gap="8rem">
-        <Flex flexDir={"column"} gap={5} color="white">
-          <Box>
-            <FormLabel> Enter Your Email</FormLabel>
-            <Input variant="filled" placeholder="Email" w={"20rem"} />
-          </Box>
-          <Box>
-            <FormLabel> Enter A Password</FormLabel>
-            <Input variant="filled" placeholder="Password" w={"20rem"} />
-          </Box>
-        </Flex>
-        <Flex flexDir={"column"} gap={5}>
-          <Box>
-            <Button
-              w={"20rem"}
-              bg="green.500"
-              color={"white"}
-              borderRadius="20"
-              _hover={{ color: "black", bg: "#E2E8F0" }}
-            >
-              Login
-            </Button>
-          </Box>
-          <Box>
-            <Button
-              w={"20rem"}
-              bg="blue.500"
-              color={"white"}
-              borderRadius="20"
-              _hover={{ color: "black", bg: "#E2E8F0" }}
-            >
-             New User? Signup
-            </Button>
-          </Box>
-        </Flex>
+      <Link to={'/'}>
+        <Image src={xsell_logo} alt="logo" maxWidth={"150px"} />
+      </Link>
+      <Box
+        p={{base:'15px',sm:'15px',md:'20px'}}
+        bgGradient="linear(to-l,#FF0080,#7928CA)"
+        boxShadow="dark-lg"
+        // border="1px solid"
+        borderRadius={10}
+        w={"fit-content"}
+
+        // pos={"absolute"}
+        // top="5rem"
+        // left={"35rem"}
+      >
+        <Flex   alignItems={'center'} flexDir='row-reverse' gap={'5rem'} mb={5}>
+      <IconButton
+      
+                aria-label="toggle theme"
+                bgColor={'blue.300'}
+                rounded="full"
+                size="xs"
+              
+                onClick={() => setTheme(!theme)}
+                icon={theme ? <FaMoon />: <FaSun color="black" />  }
+              />
+      <Heading size={'lg'} textAlign={'center'}  color='white'>Login</Heading>  
       </Flex>
+        <Flex flexDir={"column"} gap="8rem">
+          <Flex flexDir={"column"} gap={5} color="white">
+            <Box>
+              <FormLabel> Enter Your Email</FormLabel>
+              <Input variant="filled" placeholder="Email" w={{base:'16rem',sm:'20rem'}} />
+            </Box>
+            <Box>
+              <FormLabel> Enter A Password</FormLabel>
+              <Input variant="filled" placeholder="Password" w={{base:'16rem',sm:'20rem'}} />
+            </Box>
+          </Flex>
+          <Flex flexDir={"column"} gap={5}>
+            <Box>
+              <Button
+                w={{base:'16rem',sm:'20rem'}}
+                bg="green.500"
+                color={"white"}
+                borderRadius="20"
+                _hover={{ color: "black", bg: "#E2E8F0" }}
+              >
+                Login
+              </Button>
+            </Box>
+            <Box>
+              <Button
+                w={{base:'16rem',sm:'20rem'}}
+                bg="blue.500"
+                color={"white"}
+                borderRadius="20"
+                _hover={{ color: "black", bg: "#E2E8F0" }}
+              >
+                <Link to={"/signup"}> New User? Signup</Link>
+              </Button>
+            </Box>
+          </Flex>
+        </Flex>
+      </Box>
     </Box>
   );
 };
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,87 @@
+import {
+    Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Input,
+  ListItem,
+  UnorderedList,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FiMenu, FiPlus } from "react-icons/fi";
+import { MdManageAccounts } from "react-icons/md";
+import { BiLogOutCircle } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+  const [status, setStatus] = useState(false);
+  return (
+    <>
+      <Button
+        bg={"transparent"}
+        variant={"outline"}
+        ref={btnRef}
+        colorScheme="blue"
+        border={"2px solid"}
+        onClick={onOpen}
+        p="0px"
+      >
+        <FiMenu fontSize={"2rem"} color="black" />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent >
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+
+          <DrawerBody>
+            {status ? (
+              <Flex flexDir={"column"} gap={5}>
+                <Button colorScheme={"blue"}>
+                  <Link to={"/login"}>Login Or Signup</Link>
+                </Button>
+                <Button colorScheme={"whatsapp"} leftIcon={<FiPlus />}>
+                  {" "}
+                  Sell
+                </Button>
+              </Flex>
+            ) : (
+              <Flex flexDir={"column"} h={'100%'} justify={'space-between'} >
+                <Flex flexDir={"column"} gap={5}>
+                <Button colorScheme={"blue"} leftIcon={<MdManageAccounts/> }>
+                  <Link to={"/myacc"}> My Account</Link>
+                </Button>
+                <Button colorScheme={"whatsapp"} leftIcon={<FiPlus />}>
+                  {" "}
+                  Sell
+                </Button>
+                </Flex>
+                <Button colorScheme={'red'} leftIcon={<BiLogOutCircle/>} >
+                    Log Out
+                </Button>
+              </Flex>
+            )}
+          </DrawerBody>
+
+          <DrawerFooter></DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Sidebar;
